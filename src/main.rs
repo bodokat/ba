e165772bcd3d513117d36bff9efb52b4e35e7dce
Binom <bod.kato@gmail.com>
2023-07-22 14:03:52 +0200

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,70 @@
-use std::str::FromStr;
+use std::{cmp::max, str::FromStr};
 
-mod parse;
+use formula::Formula;
+
+mod formula;
+
+pub static AXIOMS: &[&str] = &[
+    "1 -> (2 -> 1)",
+    "(1 -> 2 -> 3) -> (1 -> 2) -> (1 -> 3)",
+    "(1 -> 2 -> 3) -> (2 -> 1 -> 3)",
+    "(1 -> 2) -> (-2 -> -1)",
+    "--1 -> 1",
+    "1 -> --1",
+];
 
 fn main() {
-    println!("Hello, world!");
+    let context = Context::default();
+
+    let new = context.apply_mp_all();
+    println!("{:?}", context.formulas);
+    for f in new {
+        println!("{f}");
+    }
 }
 
+struct Context {
+    formulas: Vec<Formula>,
+}
 
-fn try_apply_mp(f: &Formula, a: &Formula, b: &Formula) -> Option<Formula> {
-    let Some(subst) = try_unify(f,a)
-    else {return None};
+impl Default for Context {
+    fn default() -> Self {
+        Self {
+            formulas: AXIOMS
+                .iter()
+                .map(|s| Formula::from_str(s).unwrap())
+                .collect(),
+        }
+    }
+}
 
-    Some(b.subst(&subst))
+impl Context {
+    fn apply_mp_all(&self) -> Vec<Formula> {
+        let mut result = Vec::new();
+
+        for f in self.formulas.iter() {
+            if let Formula::Implication(a, b) = f {
+                for p in self.formulas.iter() {
+                    if let Some(new) = try_apply_mp(p, a, b) {
+                        result.push(new);
+                    }
+                }
+            }
+        }
+        result
+    }
+}
+
+/// Applies modus ponens to the formulas p and (a -> b)
+/// by attempting to unify p and a
+fn try_apply_mp(p: &Formula, a: &Formula, b: &Formula) -> Option<Formula> {
+    let mut p_disj = p.clone();
+    make_disjoint(&mut p_disj, a);
+    if let Some(subst) = try_unify(p, a) {
+        Some(b.subst(&subst))
+    } else {
+        None
+    }
 }
 
 fn occurs(x: usize, t: &Formula) -> bool {
@@ -22,17 +75,38 @@ fn occurs(x: usize, t: &Formula) -> bool {
     }
 }
 
+fn make_disjoint(a: &mut Formula, b: &Formula) {
+    fn max_var(f: &Formula) -> usize {
+        match f {
+            Formula::Var(x) => *x,
+            Formula::Implication(a, b) => max(max_var(a), max_var(b)),
+            Formula::Not(a) => max_var(a),
+        }
+    }
+    fn add_to_var(f: &mut Formula, n: usize) {
+        match f {
+            Formula::Var(x) => *x += n,
+            Formula::Implication(a, b) => {
+                add_to_var(a, n);
+                add_to_var(b, n);
+            }
+            Formula::Not(a) => add_to_var(a, n),
+        }
+    }
 
+    let m = max_var(b);
+    add_to_var(a, m);
+}
 
-fn try_unify(a: &Formula,b: &Formula) -> Option<Vec<(usize,Formula)>> {
-    try_unify_many(vec![(a.clone(),b.clone())])
+fn try_unify(a: &Formula, b: &Formula) -> Option<Vec<(usize, Formula)>> {
+    try_unify_many(vec![(a.clone(), b.clone())])
 }
 
 fn try_unify_many(mut eqs: Vec<(Formula, Formula)>) -> Option<Vec<(usize, Formula)>> {
     let mut solution = Vec::new();
     while let Some(eq) = eqs.pop() {
         match eq {
-            (Formula::Var(x), t) | (t, Formula::Var(x)) => {
+            (t, Formula::Var(x)) | (Formula::Var(x), t)   => {
                 if occurs(x, &t) {
                     return None;
                 } else {
@@ -50,52 +124,17 @@ fn try_unify_many(mut eqs: Vec<(Formula, Formula)>) -> Option<Vec<(usize, Formul
     Some(solution)
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Formula {
-    Var(usize),
-    Implication(Box<Formula>, Box<Formula>),
-    Not(Box<Formula>),
-}
 
-#[derive(Debug)]
-pub enum FormulaParseError {
-    MismatchedParen,
-    UnexpectedEOF,
-    UnexpectedChar(char)
-}
+#[cfg(test)]
+mod test {
+    use crate::try_apply_mp;
 
-impl FromStr for Formula {
-    type Err = FormulaParseError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        parse::parse(s)
+    #[test]
+    fn mp() {
+        let p1 = "1".parse().unwrap();
+        let p2 = "2".parse().unwrap();
+        let q = "1 -> 3".parse().unwrap();
+        assert_eq!(try_apply_mp(&p1, &p2, &q), Some(q))
     }
-}
-
-pub fn implication(a: Formula, b: Formula) -> Formula {
-    Formula::Implication(Box::new(a), Box::new(b))
-}
-pub fn not(a: Formula) -> Formula {
-    Formula::Not(Box::new(a))
-}
-
-impl Formula {
-    
-
-
-    fn subst(&self, s: &Vec<(usize,Formula)>) -> Formula {
-        match self {
-            Formula::Var(v) => {
-                if let Some((_,sub)) = s.iter().find(|(x,_)| x == v) {
-                    sub.clone()
-                } else {
-                    self.clone()
-                }
-            },
-            Formula::Implication(a, b) => Formula::Implication(Box::new(a.subst(s)), Box::new(b.subst(s))),
-            Formula::Not(a) => Formula::Not(Box::new(a.subst(s))),
-        }
-    }
-
-    
 }
