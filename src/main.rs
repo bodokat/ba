8e8146f29ff849c5e5048fed06c957e8d68db062
berni <bod.kato@gmail.com>
2024-09-09 15:03:07 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -33,15 +33,15 @@ fn main() {
 
     dbg!(&search);
 
-    let mut context = Context::new(&langs::ImpFalse::meredith1());
+    let mut context = Context::new(&langs::ImpFalse::church());
     let runs = args.iterations;
-    let mut file = std::fs::File::create("outputs/imp_false/meredith1.txt").unwrap();
+    // let mut file = std::fs::File::create("outputs/imp_false/meredith1.txt").unwrap();
 
     for run in 0..runs {
         context.step();
 
-        let num_entries = context.new_entries.len();
-        writeln!(file, "added in run {run}: {num_entries} new entries").unwrap();
+        let num_entries = context.entries.len();
+        // writeln!(file, "added in run {run}: {num_entries} new entries").unwrap();
         // for (formula, meta) in &context.new_entries {
         //     writeln!(
         //         file,
@@ -52,14 +52,16 @@ fn main() {
         //     )
         //     .unwrap();
         // }
-        writeln!(file).unwrap();
+        // writeln!(file).unwrap();
 
         println!("run {run}, {num_entries} new");
 
         if let Some(f) = &search {
-            if let Some(found) = context.new_entries.get(f) {
+            if let Some(found) = context.entries.get(f) {
                 println!("Found formula ({f}) after {run} iterations");
                 return;
+            } else {
+                println!("Formula ({f}) not found in iteration {run}");
             }
         }
     }
