4b52edcaf3400015fd4a2fd5bc7fcaeb6a5459cd
berni <bod.kato@gmail.com>
2025-03-22 20:35:00 +0100

    output proof

--- src/context.rs
+++ src/context.rs
@@ -3,27 +3,26 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-use hashbrown::HashSet;
-
-use ahash::RandomState;
+use ahash::HashMap;
 use rayon::prelude::*;
 
 use crate::formula::language::{modus_ponens, Language, Normal};
 
-const MAX_LEN: usize = 32;
+const MAX_LEN: usize = 64;
 
 #[derive(Debug)]
 pub struct Context<L: Language> {
-    pub entries: HashSet<Normal<L>, RandomState>,
+    pub entries: HashMap<Normal<L>, Meta>,
+    next_idx: AtomicUsize,
 }
 
-#[derive(PartialEq, Eq, Hash, Debug)]
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
 pub struct Meta {
     pub index: usize,
-    pub source: Source,
+    pub sources: Vec<Source>,
 }
 
-#[derive(PartialEq, Eq, Hash, Debug)]
+#[derive(PartialEq, Eq, Hash, Debug, Clone, Copy)]
 pub enum Source {
     Axiom,
     MP(usize, usize),
@@ -40,37 +39,68 @@ impl Display for Source {
 
 impl<L: Language> Context<L> {
     pub fn new(axioms: &[Normal<L>]) -> Self {
-        let entries = axioms.iter().cloned().collect();
+        let entries = axioms
+            .iter()
+            .enumerate()
+            .map(|(index, f)| {
+                (
+                    f.clone(),
+                    Meta {
+                        index,
+                        sources: vec![Source::Axiom],
+                    },
+                )
+            })
+            .collect::<HashMap<_, _>>();
 
-        Self { entries }
+        let next_idx = entries.len();
+        Self {
+            entries,
+            next_idx: AtomicUsize::new(next_idx),
+        }
+    }
+
+    pub fn new_entries_iter<'a, F: Fn(&Normal<L>, &Normal<L>, &Normal<L>) + Send + Sync + 'a>(
+        &'a mut self,
+        for_each_new: &'a F,
+    ) -> impl ParallelIterator<Item = (Normal<L>, Source, usize)> + 'a {
+        self.entries.par_iter().flat_map_iter(|(f1, m1)| {
+            self.entries.iter().filter_map(|(f2, m2)| {
+                modus_ponens(f1, f2)
+                    .filter(|f|
+                        // f.len() < MAX_LEN &&
+                        !self.entries.contains_key(f))
+                    .inspect(|f| for_each_new(f1, f2, f))
+                    .map(|res| {
+                        (
+                            res,
+                            Source::MP(m1.index, m2.index),
+                            self.next_idx.fetch_add(1, Ordering::Relaxed),
+                        )
+                    })
+            })
+        })
     }
 
     pub fn step<F: Fn(&Normal<L>, &Normal<L>, &Normal<L>) + Send + Sync>(
         &mut self,
-        for_each_new: F,
+        for_each_new: &F,
     ) {
-        let max_len = AtomicUsize::new(0);
-        let new_entries = self
-            .entries
-            .par_iter()
-            .flat_map_iter(|f1| {
-                self.entries.iter().filter_map(|f2| {
-                    modus_ponens(f1, f2)
-                        .inspect(|f| {
-                            max_len.fetch_max(f.len(), Ordering::Relaxed);
-                            for_each_new(f1, f2, f);
-                        })
-                        .filter(|f| f.len() <= MAX_LEN)
-                })
-            })
-            // .filter(
-            //     #[inline(never)]
-            //     |f| !self.entries.contains(f),
-            // )
-            .collect_vec_list();
+        let new_entries = self.new_entries_iter(for_each_new).collect_vec_list();
 
-        println!("max len: {}", max_len.load(Ordering::Relaxed));
-        self.entries
-            .par_extend(new_entries.into_par_iter().flatten());
+        new_entries
+            .into_iter()
+            .flatten()
+            .for_each(|(f, source, index)| {
+                let entry = self.entries.entry(f);
+                let meta = entry.or_insert(Meta {
+                    index,
+                    sources: Vec::new(),
+                });
+                if !meta.sources.contains(&source) {
+                    meta.sources.push(source);
+                }
+            });
+        // println!("max len: {}", max_len.load(Ordering::Relaxed));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 #![warn(clippy::pedantic, clippy::perf)]
 #![allow(clippy::question_mark, clippy::comparison_chain)]
 
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::{
+    io::{self, Write},
+    ops::AddAssign,
+};
 
+use ahash::{HashMap, HashMapExt};
 use clap::Parser;
 
 mod context;
 mod formula;
-use formula::langs::{self};
+use formula::langs;
 
-use context::Context;
+use context::{Context, Source};
+use itertools::Itertools;
+use rayon::iter::{ParallelDrainFull, ParallelExtend, ParallelIterator};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -20,46 +26,131 @@ struct Args {
 
     #[arg(short, long)]
     search: Option<String>,
+
+    #[arg(long)]
+    stats: Option<String>,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Args::parse();
 
     let search = args.search.map(|f| f.parse().unwrap());
 
-    let mut context = Context::new(&langs::ImpNeg::lukasiewicz1());
+    let mut context = Context::new(&langs::ImpNeg::lukasiewicz_tarski());
     let runs = args.iterations;
-    // let mut file = std::fs::File::create("outputs/imp_false/meredith1.txt").unwrap();
+
+    let mut found = None;
 
     for run in 0..runs {
-        if let Some(search) = &search {
-            let found = AtomicBool::new(false);
-            context.step(|f1, f2, f| {
-                if f == search {
-                    println!("Found formula ({f}) after {run} iterations");
-                    println!("From MP with ({f1}), ({f2})");
-                    found.store(true, Ordering::Relaxed);
-                }
-            });
-            if found.load(Ordering::Relaxed) {
+        context.step(&(|_, _, _| ()));
+
+        let num_entries = context.entries.len();
+
+        println!("run {run}, {num_entries} new");
+
+        if let Some(f) = &search {
+            if let Some(formula) = context.entries.get(f) {
+                println!("Found formula ({f}) after {run} iterations");
+                found = Some(formula.clone());
                 break;
+                // if let Source::MP(s1, s2) = found.source {
+                //     let prev = context.entries.iter().filter(|(f,m)| m.index == s1 || m.index == s2).collect();
+                // }
+            } else {
+                println!("Formula ({f}) not found in iteration {run}");
             }
         }
 
-        let num_entries = context.entries.len();
-        // writeln!(file, "added in run {run}: {num_entries} new entries").unwrap();
-        // for (formula, meta) in &context.new_entries {
-        //     writeln!(
-        //         file,
-        //         "{i}: {formula} [{source}]",
-        //         i = meta.index,
-        //         formula = formula,
-        //         source = meta.source
-        //     )
-        //     .unwrap();
+        // if let Some(search) = &search {
+        //     let found = AtomicBool::new(false);
+        //     context.step(|f1, f2, f| {
+        //         if f == search {
+        //             println!("Found formula ({f}) after {run} iterations");
+        //             println!("From MP with ({f1}), ({f2})");
+        //             found.store(true, Ordering::Relaxed);
+        //         }
+        //     });
+        //     if found.load(Ordering::Relaxed) {
+        //         break;
+        //     }
         // }
-        // writeln!(file).unwrap();
 
-        println!("run {run}, {num_entries} found");
+        println!("run {run} complete");
     }
+
+    if let Some(search) = search {
+        if let Some(found) = found {
+            let mut derivation = HashMap::new();
+            derivation.insert(found.index, (found.clone(), search));
+
+            let mut to_find: Vec<_> = found
+                .sources
+                .iter()
+                .filter_map(|s| {
+                    if let &Source::MP(a, b) = s {
+                        Some([a, b].into_iter())
+                    } else {
+                        None
+                    }
+                })
+                .flatten()
+                .collect();
+
+            while !to_find.is_empty() {
+                let new = context
+                    .entries
+                    .iter()
+                    .filter(|(_, m)| to_find.contains(&m.index))
+                    .flat_map(|(e, m)| {
+                        derivation.insert(m.index, (m.clone(), e.clone()));
+                        m.sources
+                            .iter()
+                            .filter_map(|s| {
+                                if let &Source::MP(a, b) = s {
+                                    Some([a, b].into_iter())
+                                } else {
+                                    None
+                                }
+                            })
+                            .flatten()
+                    })
+                    .collect();
+
+                to_find = new;
+            }
+
+            let mut derivation = derivation.drain().collect::<Vec<_>>();
+            derivation.sort_by_key(|(i, _)| *i);
+
+            for (i, (meta, formula)) in derivation {
+                println!("{i}: {formula} ({s})", s = meta.sources.iter().join("; "));
+            }
+        }
+    }
+
+    if let Some(path) = args.stats {
+        let mut file = std::fs::File::create(path).unwrap();
+
+        println!("generating stats...");
+
+        let stats = context
+            .new_entries_iter(&(|_, _, _| ()))
+            .fold(HashMap::new, |mut acc, val| {
+                acc.entry(val.0.len()).or_insert(0).add_assign(1);
+                acc
+            })
+            .reduce(HashMap::new, |mut a, mut b| {
+                a.par_extend(b.par_drain());
+                a
+            });
+
+        writeln!(file, "len,amount")?;
+
+        for (len, amount) in &stats {
+            writeln!(file, "{len},{amount}")?;
+        }
+
+        println!("size: {}", stats.len());
+    }
+    Ok(())
 }
